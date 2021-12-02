811c5497b82cd895d4af97c87b9a23a397fb2321
Hassanbhatti <66300348+HassanrazaBhatti@users.noreply.github.com>
2021-12-02 18:52:47 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ipanda/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ipanda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Future <void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Seller App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
+
